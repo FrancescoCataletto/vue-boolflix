870772f8c6d81ade4acec4f2af67737861f060ed
FrancescoCataletto <catalettofrancesco@gmail.com>
2022-05-20 12:40:45 +0200

    created header and main components

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,23 @@
 <template>
   <div>
     
+    <HeaderComponent />
+
+    <MainComponent />
+
   </div>
 </template>
 
 <script>
+import HeaderComponent from './components/HeaderComponent.vue';
+import MainComponent from './components/MainComponent.vue';
 
 export default {
   name: 'App',
   components: {
-    
-  }
+    HeaderComponent,
+    MainComponent
+}
 }
 </script>
 

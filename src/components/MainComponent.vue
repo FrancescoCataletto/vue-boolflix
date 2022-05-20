53a7bd02b7b6c3d3e02c99eb0d6b916274e956ca
FrancescoCataletto <catalettofrancesco@gmail.com>
2022-05-20 15:54:26 +0200

    draft structure for movie card

--- src/components/MainComponent.vue
+++ src/components/MainComponent.vue
@@ -1,15 +1,20 @@
 <template>
-  <div>
+  <main class="container">
 
-  </div>
+      <MovieCardComponent />
+
+  </main>
 </template>
 
 <script>
+import MovieCardComponent from './MovieCardComponent.vue';
 export default {
-    name: "MainComponent"
+    name: "MainComponent",
+    components: { MovieCardComponent }
 }
 </script>
 
 <style lang="scss" scoped>
+@import "../assets/global.scss";
 
 </style>
---
+++ src/components/MovieCardComponent.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="card">
+      <div class="card-body">
+          <h5>TITOLO</h5>
+          <h6>TITOLO ORIGINALE</h6>
+          <p>LINGUA</p>
+          <p>VOTO</p>
+      </div>
+  </div>
+</template>
+
+<script>
+export default {
+    name: "MovieCardComponent"
+}
+</script>
+
+<style lang="scss" scoped>
+@import "../assets/global.scss";
+
+.card{
+    width: 20%;
+}
+</style>

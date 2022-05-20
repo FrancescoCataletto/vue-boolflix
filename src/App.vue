01abba977656cb8465a2dc0b87f32705b40be97b
FrancescoCataletto <catalettofrancesco@gmail.com>
2022-05-20 13:01:29 +0200

    initialized API call in the App, all working

--- src/App.vue
+++ src/App.vue
@@ -11,13 +11,44 @@
 <script>
 import HeaderComponent from './components/HeaderComponent.vue';
 import MainComponent from './components/MainComponent.vue';
+import axios from "axios";
 
 export default {
   name: 'App',
   components: {
     HeaderComponent,
     MainComponent
+},
+
+data(){
+  return{
+    mockURL: "https://api.themoviedb.org/3/search/movie",
+    params: {
+      query: "marvel",
+      api_key: "dcc94704b9ae6a8949ee1cf936124c9f",
+      language: "it_IT"
+    }
+  }
+},
+
+methods:{
+  getAPI(){
+    axios.get(this.mockURL, {
+      params: this.params
+    })
+    .then(res => {
+      console.log(res.data.results)
+    })
+    .catch(err =>{
+      console.log(err)
+    })
+  }
+},
+
+mounted(){
+  this.getAPI()
 }
+
 }
 </script>
 
